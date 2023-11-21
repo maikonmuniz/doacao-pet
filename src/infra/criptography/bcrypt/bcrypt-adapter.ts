000395d152f283b4eac1c589046fc801c25b70e3
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-11-21 03:52:47 -0300

    fix: correção de chamada do hash do bcrypt

--- src/infra/criptography/bcrypt/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt/bcrypt-adapter.ts
@@ -2,7 +2,7 @@ import { compare, hash } from 'bcrypt'
 
 export class BcryptAdapter{
     async encrypt(value: string): Promise<string> {
-        const hashGerado = await hash.hash(value, 6)
+        const hashGerado = await hash(value, 6)
         return hashGerado
     }
 
