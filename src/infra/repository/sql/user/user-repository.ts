5efbf5f8907b4ad7ebf428832b9ffaf09e3a8d13
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-11-22 23:47:23 -0300

    refactor: repository user-repository add consulta

--- src/infra/repository/sql/user/user-repository.ts
+++ src/infra/repository/sql/user/user-repository.ts
@@ -1,14 +1,25 @@
-import MySQLAdapter from "../../../adapters/mySQL/MySQLAdapter";
+import { MySQLAdapter } from "../../../../adapters/mySQL/MySQLAdapter";
+import { repoUser } from "../../../../core/repository/user/repository-user";
 
-export class UserRepository {
+export class UserRepository implements repoUser {
     mySQLAdapter: MySQLAdapter
 
     constructor(mySQLAdapter: MySQLAdapter){
         this.mySQLAdapter = mySQLAdapter
     }
 
-    async salvar (obj) {
+    async cadastrar (obj) {
         const sql = `insert into account (nome, email, senha, cpf, idade) values ("${obj.nome}", "${obj.email}", "${obj.senha}, "${obj.cpf}", "${obj.idade}");`
         this.mySQLAdapter.query(sql)
+    }
+
+    async consultarEmail(email: string): Promise<boolean> {
+        const sql = `select email from user where ${email} = email;`
+        const existeEmail = await this.mySQLAdapter.query(sql)
+
+        if (existeEmail) {
+            return true
+        }
+        return false
     }
 }
