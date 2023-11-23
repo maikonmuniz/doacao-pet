b8f8f3d7f1ffca16ffdf3f1173acd8c0dd626b47
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-11-22 23:49:14 -0300

    refactor: ajuste de banco de dados consulta correta

--- src/infra/repository/sql/pet/repository-pet.ts
+++ src/infra/repository/sql/pet/repository-pet.ts
@@ -1,4 +1,4 @@
-import MySQLAdapter from "../../../../adapters/mySQL/MySQLAdapter";
+import { MySQLAdapter } from "../../../../adapters/mySQL/MySQLAdapter";
 import { Pet } from "../../../../core/entities/pet/Pet";
 
 export class PetRepository {
@@ -9,7 +9,8 @@ export class PetRepository {
     }
 
     async salvar(obj: Pet) {
-        const sql = `insert into account (nome, cor, raca, idade) values ("${obj.cor}", "${obj.raca}", "${obj.idade}");`
+        const sql = `insert into pet (nome, cor, raca, idade) values ("${obj.cor}", "${obj.raca}", "${obj.idade}");`
+        // const pet=new Pet(obj.nome, obj.raca, obj.idade, obj.castrado, obj.cor)
         this.mySQLAdapter.query(sql)
     }
 }
