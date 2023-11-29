3493af6b33f0b9382eeaebe047551f3b0ec25dfe
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-11-28 21:41:26 -0300

    feat: privano construtor e retornando em um metodo

--- src/core/entities/user/User.ts
+++ src/core/entities/user/User.ts
@@ -1,18 +1,27 @@
 
 export class User {
-    constructor(readonly props: Props) {
-        this.props = props
+
+    constructor(readonly nome: string,
+                readonly email: string,
+                readonly senha: string,
+                readonly cpf: string,
+                readonly idade: number,
+                readonly data) 
+                { }
+
+    static criacao(nome: string, 
+                   email: string,
+                   senha: string,
+                   cpf: string, 
+                   idade: number) {
+
+        const data: Date = new Date()
+        return new User(nome, email, senha, cpf, idade, data)
     }
 
-    verificarIdadeValida (): boolean {
-        return (this.props.idade > 18)
+    verificarIdadeValida () {
+        const minimaIdadeValida = 18
+        if (this.idade < minimaIdadeValida) return false
+        return true
     }
-}
-
-type Props = {
-    nome: string,
-    email: string,
-    senha: string,
-    cpf: string,
-    idade: number,
 }
