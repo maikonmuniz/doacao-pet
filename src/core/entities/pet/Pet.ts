7cb34eca1169f7383c4cafa03017f8899135c4be
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-12-03 00:29:01 -0300

    feat: add metodo de criação

--- src/core/entities/pet/Pet.ts
+++ src/core/entities/pet/Pet.ts
@@ -1,7 +1,22 @@
 
 export class Pet {
-    doado: boolean;
 
-    constructor(readonly nome, readonly raca,  readonly idade, readonly castrado, readonly cor) {
-     }
+    private constructor(
+                readonly id,
+                readonly nome, 
+                readonly raca,
+                readonly idade, 
+                readonly castrado, 
+                readonly cor,
+                readonly agressivo,
+                readonly doado) {
+    }
+
+    static criacao(id: number, nome: string, raca: string, idade: number, castrado: boolean, cor: string, agressivo: boolean, doado: boolean) {
+        if (!doado) {
+            doado = false
+        }
+
+        return new Pet(id, nome, raca, idade, castrado, cor, agressivo, doado)
+    }
 }
