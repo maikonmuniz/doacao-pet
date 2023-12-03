187ca6d3b1b2e638550bfeaec05169847ca7ba97
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-12-02 23:03:30 -0300

    feat: add methodo no repository consultar usuário

--- src/core/repository/user/repository-user.ts
+++ src/core/repository/user/repository-user.ts
@@ -1,6 +1,7 @@
 import { User } from "../../entities/user/User"
 
 export interface repoUser{
-    consultarEmail(email: string): Promise<boolean>
+    consultarEmail(email: string): Promise<User>
     cadastrar(input: User): Promise<void>
+    consultar(id: number)
 }
